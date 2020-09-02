3e20bb1a0d76f5c4a95b3a2a58541e0c916f4b92
the-notable <danielwkennedy@outlook.com>
2020-09-01 20:54:37 -0700

    try to debug

--- src/queryable/mod.rs
+++ src/queryable/mod.rs
@@ -304,6 +304,8 @@ impl Queryable for Conn {
         let params = params.into();
         BoxFuture(Box::pin(async move {
             let statement = self.get_statement(stmt).await?;
+            println!("{:#?}", statement);
+            println!("{:#?}", params);
             self.execute_statement(&statement, params).await?;
             Ok(QueryResult::new(self))
         }))
